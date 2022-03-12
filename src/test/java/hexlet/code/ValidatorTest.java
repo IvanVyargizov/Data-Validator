2b82afbf1f3f5ffafb063913d81fde0ba021867a
Ivan_Vyargizov <vyargizov@gmail.com>
2022-03-13 00:27:30 +0400

    update

--- src/test/java/hexlet/code/ValidatorTest.java
+++ src/test/java/hexlet/code/ValidatorTest.java
@@ -1,14 +1,14 @@
 package hexlet.code;
 
-import hexlet.code.schemas.BaseSchema;
-import hexlet.code.schemas.MapSchema;
-import hexlet.code.schemas.NumberSchema;
+//import hexlet.code.schemas.BaseSchema;
+//import hexlet.code.schemas.MapSchema;
+//import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
-import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
